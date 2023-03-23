dfdf4e825aa730713e96ec99302e59ffbf39e5d4
Prince Musonda <115486288+prince-musonda@users.noreply.github.com>
2023-03-23 14:56:22 +0200

    initial commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var body = document.querySelector('body');
 var colorPickerOne = document.querySelector('#color-picker-one');
 var colorPickerTwo = document.querySelector('#color-picker-two');
+var randomColorBtn = document.querySelector('button')
 var h3 = document.querySelector('h3');
 
 //initial values
@@ -13,5 +14,21 @@ function changeBackground(){
     h3.textContent = `linear-gradient(90deg, ${colorPickerOne.value},${colorPickerTwo.value})`
 }
 
+function generateRandomColors(){
+    var red1 = Math.floor(Math.random() * 256);
+    var green1 = Math.floor(Math.random() * 256);
+    var blue1 = Math.floor(Math.random() * 256);
+
+    return `rgb(${red1},${green1},${blue1})`
+}
+
+function changeToRandomBackground(){
+    var color1 = generateRandomColors();
+    var color2 = generateRandomColors();
+    body.style.background = `linear-gradient(90deg, ${color1},${color2})`
+    h3.textContent = body.style.background + ';'
+}
+
 colorPickerOne.addEventListener('input',changeBackground);
 colorPickerTwo.addEventListener('input',changeBackground);
+randomColorBtn.addEventListener('click',changeToRandomBackground)
